37637178fce9ade9fc2dc1279d2c39a9ab505fee
ahmedyouseeff <ahmedyouseeff9@gmail.com>
2023-05-05 16:38:00 +0300

    project has been added to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ViewChild,
+  ChangeDetectorRef,
+  OnInit,
+} from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+import { MatSidenav } from '@angular/material/sidenav';
+import { NewsService } from './service/news.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
   title = 'NewsApp';
+
+  public sources: any = [];
+  public articles: any = [];
+  public selectedNewsChannel: string = 'Top 10 trending News!';
+
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(
+    private observer: BreakpointObserver,
+    private cdr: ChangeDetectorRef,
+    private newsApi: NewsService
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:787px)']).subscribe((res) => {
+      if (res?.matches) {
+        this.sideNav.mode = 'over';
+        this.sideNav.close();
+      } else {
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    });
+    this.cdr.detectChanges();
+  }
+
+  ngOnInit(): void {
+    this.newsApi.initArticles().subscribe((res: any) => {
+      this.articles = res.articles;
+    });
+
+    this.newsApi.initSources().subscribe((res: any) => {
+      this.sources = res.sources;
+    });
+  }
+
+  searchSource(source: any) {
+    this.newsApi.getArticlesByID(source.id).subscribe((res: any) => {
+      this.articles = res.articles;
+      this.selectedNewsChannel = source.name;
+    });
+  }
 }
